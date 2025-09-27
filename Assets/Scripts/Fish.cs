b49b5d1c7787d4a3e0ccd85a7664c1db95dd8404
Helena_S <helmarsav@gmail.com>
2025-09-27 12:46:43 +0200

    setup, fish, obstacle move

--- Assets/Scripts/Fish.cs
+++ Assets/Scripts/Fish.cs
@@ -2,15 +2,13 @@ using UnityEngine;
 
 public class Fish : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        gameObject.transform.Rotate(0, 1, 0);
     }
 }
